30d4e7789b60e77543e7e0000d3c7de02ee4b576
Issaias <106002902+fluffy395@users.noreply.github.com>
2022-10-09 03:24:51 -0500

    Add files via upload

---
+++ Marco Selección Múltiple/MultipleSelectionTest.java
@@ -0,0 +1,15 @@
+package IntFces;
+
+// Fig. 12.26: MultipleSelectionTest.java
+// MultipleSelectionFrame test.
+
+import javax.swing.JFrame;
+
+public class MultipleSelectionTest {
+    public static void main(String[] args) {
+        MultipleSelectionFrame multipleSelectionFrame = new MultipleSelectionFrame();
+        multipleSelectionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        multipleSelectionFrame.setSize(800,600);
+        multipleSelectionFrame.setVisible(true);
+    }
+}
